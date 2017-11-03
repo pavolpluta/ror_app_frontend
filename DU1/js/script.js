9a03f79bc180fe490072e2588e0ad9038a0f6566
Pauli <pavol.pluta1@gmail.com>
2017-11-04 00:45:08 +0100

    Most of the Front-end part done

---
+++ DU1/js/script.js
@@ -0,0 +1,14 @@
+
+//Filtering
+$(document).ready(function(){
+    $("#searchBuilding").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $(".buildings a,.rooms a").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+
+// function to merge <td> with same values in Schedule would be something I'd like to implement when
+// I'll have skills to do that
+
